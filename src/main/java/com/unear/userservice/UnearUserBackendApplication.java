2a77ec351962699403e65851fb5bb9f4ba4a377a
1seyoung <akdlrnal0705@gmail.com>
2025-07-08 14:48:14 +0900

    init: initial commit

---
+++ src/main/java/com/unear/userservice/UnearUserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unear.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnearUserBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnearUserBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/unear/userservice/UnearUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unear.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnearUserBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
