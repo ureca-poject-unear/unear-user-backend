27ff75c79a9d2f9268e51a13b9202fac69b2b899
lbk00 <lbk6661@gmail.com>
2025-07-14 16:45:47 +0900

    feat : 장소 dto 추가

---
+++ src/main/java/com/unear/userservice/place/dto/request/PlaceRequestDto.java
@@ -0,0 +1,16 @@
+package com.unear.userservice.place.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceRequestDto {
+    private String categoryCode;
+    private String benefitCategory;
+    private Boolean isFavorite;
+}
