5d3fd0a24afb75651e6efeb7fdefe1b43931cc8b
tjdqls3607 <tjdqls3124@gmail.com>
2025-07-14 15:56:48 +0900

    feat: 사용자 바코드 조회

--- src/main/java/com/unear/userservice/user/UserController.java
+++ src/main/java/com/unear/userservice/user/UserController.java
@@ -1,9 +1,29 @@
 package com.unear.userservice.user;
 
+import com.unear.userservice.common.response.ApiResponse;
+import com.unear.userservice.common.security.CustomUser;
+import com.unear.userservice.exception.exception.UserNotFoundException;
+import com.unear.userservice.user.entity.User;
+import com.unear.userservice.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/me/barcode")
+    public ApiResponse<String> getMyBarcode(@AuthenticationPrincipal CustomUser user) {
+
+        User dbuser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+        return ApiResponse.success("바코드 조회 성공", dbuser.getBarcodeNumber());
+    }
 
 
 }
