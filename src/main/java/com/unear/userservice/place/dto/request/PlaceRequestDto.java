0bbcf3bba80dcb3065bf7849f2a40405eb0d242f
lbk00 <lbk6661@gmail.com>
2025-07-15 13:13:47 +0900

    feat : 좌하단 , 우상단 위도 경도 파라미터 추가

--- src/main/java/com/unear/userservice/place/dto/request/PlaceRequestDto.java
+++ src/main/java/com/unear/userservice/place/dto/request/PlaceRequestDto.java
@@ -13,4 +13,9 @@ public class PlaceRequestDto {
     private String categoryCode;
     private String benefitCategory;
     private Boolean isFavorite;
+
+    private Double minLatitude;  // 좌하단 위도
+    private Double minLongitude; // 좌하단 경도
+    private Double maxLatitude;  // 우상단 위도
+    private Double maxLongitude; // 우상단 경도
 }
