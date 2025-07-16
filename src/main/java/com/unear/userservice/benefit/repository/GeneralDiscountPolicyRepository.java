467a08342196b0dcc3c5d61d917bfb1e704bd410
lbk00 <lbk6661@gmail.com>
2025-07-16 15:32:37 +0900

    feat : import 수정

--- src/main/java/com/unear/userservice/benefit/repository/GeneralDiscountPolicyRepository.java
+++ src/main/java/com/unear/userservice/benefit/repository/GeneralDiscountPolicyRepository.java
@@ -1,7 +1,7 @@
 package com.unear.userservice.benefit.repository;
 
 import com.unear.userservice.benefit.entity.GeneralDiscountPolicy;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
