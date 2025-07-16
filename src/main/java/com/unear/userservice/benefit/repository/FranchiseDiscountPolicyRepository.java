4eabdce8b4044179410fdedfe9cea572c8a5758c
lbk00 <lbk6661@gmail.com>
2025-07-16 19:25:41 +0900

    feat : franchiseId에 해당하는 모든 할인 정책 id 조회 쿼리

---
+++ src/main/java/com/unear/userservice/benefit/repository/FranchiseDiscountPolicyRepository.java
@@ -0,0 +1,16 @@
+package com.unear.userservice.benefit.repository;
+
+import com.unear.userservice.benefit.entity.FranchiseDiscountPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface FranchiseDiscountPolicyRepository extends JpaRepository<FranchiseDiscountPolicy, Long> {
+
+    @Query("SELECT f.franchiseDiscountPolicyId FROM FranchiseDiscountPolicy f WHERE f.franchise.franchiseId = :franchiseId")
+    List<Long> findPolicyIdsByFranchiseId(@Param("franchiseId") Long franchiseId);
+
+}
