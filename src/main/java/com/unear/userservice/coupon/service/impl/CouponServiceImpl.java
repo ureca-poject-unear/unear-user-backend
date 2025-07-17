4c65d82a7f200ef438888b62d62e7faf04b0b511
lbk00 <lbk6661@gmail.com>
2025-07-17 17:08:03 +0900

    feat : 쿠폰 다운로드 및 다운받은 쿠폰 바코드 생성

--- src/main/java/com/unear/userservice/coupon/service/impl/CouponServiceImpl.java
+++ src/main/java/com/unear/userservice/coupon/service/impl/CouponServiceImpl.java
@@ -2,21 +2,33 @@ package com.unear.userservice.coupon.service.impl;
 
 import com.unear.userservice.benefit.repository.FranchiseDiscountPolicyRepository;
 import com.unear.userservice.benefit.repository.GeneralDiscountPolicyRepository;
+import com.unear.userservice.common.enums.CouponStatus;
 import com.unear.userservice.common.enums.DiscountPolicy;
 import com.unear.userservice.common.enums.PlaceType;
 import com.unear.userservice.coupon.dto.response.CouponResponseDto;
+import com.unear.userservice.coupon.dto.response.UserCouponResponseDto;
 import com.unear.userservice.coupon.entity.CouponTemplate;
+import com.unear.userservice.coupon.entity.UserCoupon;
 import com.unear.userservice.coupon.repository.CouponTemplateRepository;
 import com.unear.userservice.coupon.repository.UserCouponRepository;
 import com.unear.userservice.coupon.service.CouponService;
+import com.unear.userservice.exception.exception.BarcodeDuplicatedException;
+import com.unear.userservice.exception.exception.CouponAlreadyDownloadedException;
+import com.unear.userservice.exception.exception.CouponTemplateNotFoundException;
+import com.unear.userservice.exception.exception.UserNotFoundException;
 import com.unear.userservice.place.repository.PlaceRepository;
+import com.unear.userservice.user.entity.User;
+import com.unear.userservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +39,7 @@ public class CouponServiceImpl implements CouponService {
     private final PlaceRepository placeRepository;
     private final CouponTemplateRepository couponTemplateRepository;
     private final UserCouponRepository userCouponRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
@@ -61,5 +74,47 @@ public class CouponServiceImpl implements CouponService {
                 })
                 .toList();
     }
+
+
+    @Override
+    @Transactional
+    public UserCouponResponseDto downloadCoupon(Long userId, Long couponTemplateId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+
+        CouponTemplate template = couponTemplateRepository.findById(couponTemplateId)
+                .orElseThrow(() -> new CouponTemplateNotFoundException("쿠폰 템플릿을 찾을 수 없습니다."));
+
+        UserCoupon userCoupon = UserCoupon.builder()
+                .user(user)
+                .couponTemplate(template)
+                .createdAt(LocalDate.now())
+                .couponStatusCode(CouponStatus.UNUSED.getCode())
+                .barcodeNumber(generateUniqueBarcode())
+                .build();
+
+        try {
+            userCouponRepository.save(userCoupon);
+        } catch (DataIntegrityViolationException e) {
+            throw new CouponAlreadyDownloadedException("이미 다운로드한 쿠폰입니다.");
+        }
+
+        return UserCouponResponseDto.from(userCoupon);
+    }
+
+    private String generateUniqueBarcode() {
+        for (int i = 0; i < 3; i++) {
+            String barcode = generateBarcode();
+            if (!userCouponRepository.existsByBarcodeNumber(barcode)) {
+                return barcode;
+            }
+        }
+        throw new BarcodeDuplicatedException("중복된 바코드로 인해 바코드 생성 실패");
+    }
+
+    private String generateBarcode() {
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 16);
+    }
+
 
 }
