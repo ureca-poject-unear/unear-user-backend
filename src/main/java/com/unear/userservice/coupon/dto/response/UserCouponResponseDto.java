cc766ce3ee64871d1b3ab7074a337a0325fc9582
lbk00 <lbk6661@gmail.com>
2025-07-17 17:08:59 +0900

    feat : 다운받은 쿠폰 dto 추가

---
+++ src/main/java/com/unear/userservice/coupon/dto/response/UserCouponResponseDto.java
@@ -0,0 +1,28 @@
+package com.unear.userservice.coupon.dto.response;
+
+import com.unear.userservice.coupon.entity.UserCoupon;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class UserCouponResponseDto {
+    private Long userCouponId;
+    private String couponName;
+    private String barcodeNumber;
+    private String couponStatusCode;
+    private LocalDate createdAt;
+
+    public static UserCouponResponseDto from(UserCoupon userCoupon) {
+        return UserCouponResponseDto.builder()
+                .userCouponId(userCoupon.getUserCouponId())
+                .couponName(userCoupon.getCouponTemplate().getCouponName())
+                .barcodeNumber(userCoupon.getBarcodeNumber())
+                .couponStatusCode(userCoupon.getCouponStatusCode())
+                .createdAt(userCoupon.getCreatedAt())
+                .build();
+    }
+}
+
