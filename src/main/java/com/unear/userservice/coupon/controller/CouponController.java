15f5b2c52944991bf7a3313aa0e594023ff03d54
lbk00 <lbk6661@gmail.com>
2025-07-17 17:07:06 +0900

    feat : 쿠폰 템플릿 id로 쿠폰 다운로드 api

--- src/main/java/com/unear/userservice/coupon/controller/CouponController.java
+++ src/main/java/com/unear/userservice/coupon/controller/CouponController.java
@@ -4,6 +4,7 @@ package com.unear.userservice.coupon.controller;
 import com.unear.userservice.common.response.ApiResponse;
 import com.unear.userservice.common.security.CustomUser;
 import com.unear.userservice.coupon.dto.response.CouponResponseDto;
+import com.unear.userservice.coupon.dto.response.UserCouponResponseDto;
 import com.unear.userservice.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -30,8 +31,15 @@ public class CouponController {
         return ResponseEntity.ok(ApiResponse.success("쿠폰 템플릿 조회 성공", result));
     }
 
-    // {쿠폰템플릿 id}/download
-    // 다운로드하면, unused 인채로 user_coupon
+    @PostMapping("/{couponTemplateId}/download")
+    public ResponseEntity<ApiResponse<UserCouponResponseDto>> downloadCoupon(
+            @PathVariable Long couponTemplateId,
+            @AuthenticationPrincipal CustomUser user
+    ) {
+        Long userId = (user != null && user.getUser() != null) ? user.getUser().getUserId() : null;
+        UserCouponResponseDto response = couponService.downloadCoupon(userId, couponTemplateId);
+        return ResponseEntity.ok(ApiResponse.success("쿠폰 다운로드 성공", response));
+    }
 
 }
 
