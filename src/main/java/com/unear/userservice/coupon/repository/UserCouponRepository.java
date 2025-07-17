9c9f77fba6b807692c01736fc10700ad228e9494
lbk00 <lbk6661@gmail.com>
2025-07-17 17:18:54 +0900

    chore : import 삭제

--- src/main/java/com/unear/userservice/coupon/repository/UserCouponRepository.java
+++ src/main/java/com/unear/userservice/coupon/repository/UserCouponRepository.java
@@ -1,8 +1,6 @@
 package com.unear.userservice.coupon.repository;
 
-import com.unear.userservice.coupon.entity.CouponTemplate;
 import com.unear.userservice.coupon.entity.UserCoupon;
-import com.unear.userservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
