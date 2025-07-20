9a9512d11097fdc764d98274f9a0b4fabd2ce477
lbk00 <lbk6661@gmail.com>
2025-07-20 13:38:19 +0900

    feat : 사용자 위도,경도 request dto 추가

---
+++ src/main/java/com/unear/userservice/place/dto/request/NearbyPlaceRequestDto.java
@@ -0,0 +1,12 @@
+package com.unear.userservice.place.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NearbyPlaceRequestDto {
+    private Double latitude;
+    private Double longitude;
+}
+
