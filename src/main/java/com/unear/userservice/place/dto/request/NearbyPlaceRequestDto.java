58ecfb7127b26261d975d83181ca5c3f51601b93
lbk00 <lbk6661@gmail.com>
2025-07-22 14:49:11 +0900

    feat : 시용자 위도,경도 @Schema 추가

--- src/main/java/com/unear/userservice/place/dto/request/NearbyPlaceRequestDto.java
+++ src/main/java/com/unear/userservice/place/dto/request/NearbyPlaceRequestDto.java
@@ -1,5 +1,6 @@
 package com.unear.userservice.place.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
@@ -13,11 +14,13 @@ public class NearbyPlaceRequestDto {
     @NotNull(message = "위도는 필수입니다.")
     @DecimalMin(value = "-90.0", inclusive = true, message = "위도는 -90 이상이어야 합니다.")
     @DecimalMax(value = "90.0", inclusive = true, message = "위도는 90 이하이어야 합니다.")
+    @Schema(description = "사용자 현재 위치의 위도", example = "37.5665")
     private Double latitude;
 
     @NotNull(message = "경도는 필수입니다.")
     @DecimalMin(value = "-180.0", inclusive = true, message = "경도는 -180 이상이어야 합니다.")
     @DecimalMax(value = "180.0", inclusive = true, message = "경도는 180 이하이어야 합니다.")
+    @Schema(description = "사용자 현재 위치의 경도", example = "126.9780")
     private Double longitude;
 }
 
