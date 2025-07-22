d7c90bac18953c205cbe9ca408b5fea6cc6b722f
lbk00 <lbk6661@gmail.com>
2025-07-22 10:15:50 +0900

    feat : swagger 화이트리스트 추가, filterChain cors 추가

--- src/main/java/com/unear/userservice/common/config/SecurityConfig.java
+++ src/main/java/com/unear/userservice/common/config/SecurityConfig.java
@@ -43,12 +43,16 @@ public class SecurityConfig {
 
     private static final String[] WHITE_LIST = {
             "/auth/**",
-            "/oauth2/**"
+            "/oauth2/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
     };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
