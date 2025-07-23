3fbfa4b19fa2fc3dc60eca8edcd206ecd274400f
lbk00 <lbk6661@gmail.com>
2025-07-23 17:14:56 +0900

    fix : swagger 배포서버 url 수정

--- src/main/java/com/unear/userservice/common/config/SecurityConfig.java
+++ src/main/java/com/unear/userservice/common/config/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:4000" , "http://localhost:4000", "https://dev.unear.site"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:4000" , "http://localhost:4000", "http://dev.unear.site"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/unear/userservice/common/config/SwaggerConfig.java
+++ src/main/java/com/unear/userservice/common/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
                 .name("Authorization");
 
         Server server = new Server()
-                .url("https://dev.unear.site")
+                .url("http://dev.unear.site")
                 .description("배포 서버");
 
         return new OpenAPI()
