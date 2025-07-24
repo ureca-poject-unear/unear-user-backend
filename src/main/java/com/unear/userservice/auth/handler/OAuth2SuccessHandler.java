6460a2d39c1cff1c978d6950dcad6e06c75cc7d1
lbk00 <lbk6661@gmail.com>
2025-07-24 14:15:40 +0900

    fix : onAuthenticationSuccess -> AuthServiceImpl 의존성 제거

--- src/main/java/com/unear/userservice/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/unear/userservice/auth/handler/OAuth2SuccessHandler.java
@@ -1,7 +1,9 @@
 package com.unear.userservice.auth.handler;
 
+import com.unear.userservice.auth.CookieUtil;
 import com.unear.userservice.auth.dto.response.LoginResponseDto;
 import com.unear.userservice.auth.service.AuthService;
+import com.unear.userservice.common.exception.exception.UserNotFoundException;
 import com.unear.userservice.common.jwt.JwtTokenProvider;
 import com.unear.userservice.common.jwt.RefreshTokenService;
 import com.unear.userservice.common.response.ApiResponse;
@@ -13,6 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,7 +32,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenService refreshTokenService;
-    private final AuthService authService;
 
     @Value("${app.frontend.base-url:http://localhost:4000}")
     private String frontendBaseUrl;
@@ -42,15 +44,22 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String email = oAuth2User.getAttribute("email");
 
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("유저 정보 없음")); // usernotfound 로 바꾸기
+                .orElseThrow(() -> new UserNotFoundException("유저 정보 없음"));
 
-        ApiResponse<LoginResponseDto> loginResponse = authService.oauthLogin(user, response);
-        String accessToken = loginResponse.getData().getAccessToken();
+        CustomUser customUser = new CustomUser(user);
+        String accessToken = jwtTokenProvider.generateAccessToken(customUser);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(customUser);
+
+        refreshTokenService.saveRefreshToken(user.getUserId(), refreshToken);
+
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
 
         String redirectUrl = String.format("%s/login/oauth2/code/google?accessToken=%s",
                 frontendBaseUrl, accessToken);
 
         response.sendRedirect(redirectUrl);
-
     }
 }
