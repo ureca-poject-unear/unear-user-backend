8407e895d76a87e2a698c4758ac9c24b8ee98111
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-07-25 15:06:18 +0900

    refactor: 인증헤더 Null 검증

--- src/main/java/com/unear/userservice/common/resolver/LoginUserArgumentResolver.java
+++ src/main/java/com/unear/userservice/common/resolver/LoginUserArgumentResolver.java
@@ -1,6 +1,7 @@
 package com.unear.userservice.common.resolver;
 
 import com.unear.userservice.common.annotation.LoginUser;
+import com.unear.userservice.common.exception.exception.InvalidTokenException;
 import com.unear.userservice.user.entity.User;
 import com.unear.userservice.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,7 +31,13 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
                                   NativeWebRequest webRequest,
                                   org.springframework.web.bind.support.WebDataBinderFactory binderFactory) {
         HttpServletRequest request = ((ServletWebRequest) webRequest).getRequest();
-        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
+        
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new InvalidTokenException("유효하지 않은 인증 헤더입니다");
+        }
+
+        String accessToken = authHeader.replace("Bearer ", "");
         return authService.getUserFromAccessToken(accessToken);
     }
 }
